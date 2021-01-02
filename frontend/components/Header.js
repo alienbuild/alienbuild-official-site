92261bbff5dc9c5048676ecb37f0f032249c90e4
LK <lk@alienbuild.uk>
2021-01-02 14:06:11 +0000

    Setup React Next

---
+++ frontend/components/Header.js
@@ -0,0 +1,11 @@
+import { APP_NAME } from '../config'
+
+const Header = () => {
+    return(
+        <header>
+            Header Component {APP_NAME}
+        </header>
+    )
+}
+
+export default Header
---
+++ frontend/components/Layout.js
@@ -0,0 +1,13 @@
+import Header from "./Header";
+
+const Layout = ({ children }) => {
+    return(
+        <>
+            <Header />
+            {children}
+            <footer>Footer</footer>
+        </>
+    )
+}
+
+export default Layout;
