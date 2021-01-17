0089efbcff1b91a3417f86fb3ce3004ccdcc988a
LK <lk@alienbuild.uk>
2021-01-17 11:23:15 +0000

    Render single blog page

--- frontend/pages/blogs/[slug].js
+++ frontend/pages/blogs/[slug].js
@@ -1,19 +1,43 @@
 import Head from "next/head"
 import Link from "next/link"
-import { withRouter } from 'next/router'
+import moment from "moment";
 import Layout from "../../components/Layout"
 import { useState } from 'react'
 import { singleBlog } from "../../actions/blog"
 import { API, DOMAIN, APP_NAME } from '../../config'
+import renderHTML from 'react-render-html'
 
-const SingleBlog = ({ blog, router }) => {
+const SingleBlog = ({ blog }) => {
+
+    const showBlogCategories = blog => {
+        return blog.categories.map((category, index) => (
+            <Link key={index} href={`/categories/${category.slug}`}>
+                <a>{category.name}</a>
+            </Link>
+        ))
+    }
+
+    const showBlogTags = blog => {
+        return blog.tags.map((tags, index) => (
+            <Link key={index} href={`/tags/${tag.slug}`}>
+                <a>{tag.name}</a>
+            </Link>
+        ))
+    }
+
     return(
         <>
             <Layout>
                 <main>
                     <article>
+                        <header>
+                            <img src={`${API}/blog/photo/${blog.slug}`} alt={blog.title} />
+                            Written by {blog.author.name} | Published {moment(blog.updatedAt).fromNow()}
+                            {showBlogCategories(blog)}
+                            {showBlogTags(blog)}
+                        </header>
                         <section>
-                            {JSON.stringify(blog)}
+                            {renderHTML(blog.body)}
                         </section>
                     </article>
                 </main>
@@ -33,4 +57,4 @@ SingleBlog.getInitialProps = ({query}) => {
         })
 }
 
-export default withRouter(SingleBlog)
+export default SingleBlog
